cbaae0fc64e6770441aa3aa88617e5a01ab99827
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-16 10:27:19 +0100

    clean up

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from '../images/logo.svg'
 
 export default function Nav(){
     return(
         <nav>
-            <img src={require('../images/logo.png')} alt="travel journal logo"/>
+            <img src={Logo} alt="travel journal logo"/>
             <h2>my travel journal</h2>
         </nav>
     )
