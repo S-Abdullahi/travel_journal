1f3e0f946a5522839f8b68f79ef2280d7858bb2e
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-16 10:18:37 +0100

    final commit

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Nav from './Nav'
 import Card from './Card'
+import Data from '../Data'
 
 export default function App(){
+    const displayCard = Data.map((item)=>{
+        return <Card 
+            key = {item.title}
+            {...item}
+        />
+    })
+
     return (
         <>
             <Nav/>
-            <Card/>
+            {displayCard}
         </>
     )
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -2,23 +2,22 @@ import React from "react";
 import MountFiji from '../images/mountfiji.svg'
 import LocationIcon from '../images/location_icon.svg'
 
-export default function Card(){
+export default function Card(props){
+    console.log(props)
     return (
         <section className="card">
             <div className="card__image-con">
-                <img src={MountFiji} className="card__image"/>
+                <img src={props.imageUrl} className="card__image"/>
             </div>
             <div className="card__detail">
                 <div className="card__head">
                     <img src={LocationIcon}/>
-                    <span className="card__country">Japan</span>
-                    <span className="card__link"><a href="#">View on Google Maps</a></span>
+                    <span className="card__country">{props.location}</span>
+                    <span className="card__link"><a href={props.googleMapsUrl}>View on Google Maps</a></span>
                 </div>
-                <h3 className="card__title">Mount Fuji</h3>
-                <p className="card__date">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="card__description">
-                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </p>
+                <h3 className="card__title">{props.title}</h3>
+                <p className="card__date">{props.startDate} - {props.endDate}</p>
+                <p className="card__description">   {props.description}</p>
             </div>
         </section>
     )
